aa857f609ad55b685ef7f7ba3d94881a5322625d
Hakan Karaahmetoglu <81388445+hakankaraahmet@users.noreply.github.com>
2022-07-23 00:21:09 +0300

    SideBar bug is fixed

--- components/SideBar/index.tsx
+++ components/SideBar/index.tsx
@@ -13,34 +13,34 @@ const NavBar = ({
     <div
       className={`${
         isOpenSideBar ? "block z-20 " : "hidden"
-      } h-screen absolute w-full bg-indigo-900 md:w-64 md:block md:relative `}
+      } min-h-screen fixed w-full bg-indigo-900 md:w-64 md:block md:relative `}
     >
       <div className="flex justify-between bg-indigo-800 ">
         <div className="p-5 text-xl font-bold tracking-wide text-gray-200 uppercase">
           Super Corp
         </div>
         <div
-          className={`${
-            isOpenSideBar ? "block" : "hidden"
-          } md:hidden p-4 `}
+          className={`${isOpenSideBar ? "block" : "hidden"} md:hidden p-4 `}
           onClick={() => setIsOpenSideBar(false)}
         >
           <Image src="/Logos/close.png" alt="cross" width={40} height={40} />
         </div>
       </div>
-      <div>
-        <div className="p-5 text-xs font-bold tracking-wide text-gray-200 uppercase">
-          Pages
-        </div>
-      </div>
-      {navbarItems.map((item, i) => (
-        <div className="flex m-5 mt-2 cursor-pointer" key={i}>
-          <div>
-            <Image src={item.image} alt="logo" width={20} height={20} />
+      <div className="fixed">
+        <div>
+          <div className="p-5 text-xs font-bold tracking-wide text-gray-200 uppercase">
+            Pages
           </div>
-          <div className="mx-3 text-sm text-gray-100">{item.title}</div>
         </div>
-      ))}
+        {navbarItems.map((item, i) => (
+          <div className="flex m-5 mt-2 cursor-pointer" key={i}>
+            <div>
+              <Image src={item.image} alt="logo" width={20} height={20} />
+            </div>
+            <div className="mx-3 text-sm text-gray-100">{item.title}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
